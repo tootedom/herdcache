5487b4ab14d52d84c5570fc53d0162d428a35199
dominictootell <dominic.tootell@gmail.com>
2016-09-25 19:17:38 +0100

    fix cache compare

--- src/test/java/org/greencheek/caching/herdcache/memcached/TestSimpleMemcachedCaching.java
+++ src/test/java/org/greencheek/caching/herdcache/memcached/TestSimpleMemcachedCaching.java
@@ -1459,7 +1459,7 @@ public class TestSimpleMemcachedCaching {
         MetricRegistry registry = new MetricRegistry();
 
         Predicate<String> cachedValueAllowed = (String value) -> {
-            return value.equals("cacheable");
+            return value.equals("non_cacheable");
         };
 
         cache = new SpyMemcachedCache<>(
